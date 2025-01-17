274a17dd8637182e3a2a3c25f2978da8436e1a93
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-01-17 18:49:15 +0100

    Enabled the CORS suuport for the Pokemon Controller

--- src/main/java/ey/codemotion/hexagonal/infrastructure/controller/PokemonController.java
+++ src/main/java/ey/codemotion/hexagonal/infrastructure/controller/PokemonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 @Tag(name = "Pokemons", description = "API for Pokemons")
 public class PokemonController {
